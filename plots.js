3747aba303e10dd5c767ba45d348ce730385d001
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-02 10:42:20 -0700

    update index file

--- plots.js
+++ plots.js
@@ -1,36 +1,15 @@
 function init() {
-  data = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }];
-  Plotly.newPlot("plot", data);
-};
-
-d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+  
 function updatePlotly() {
-  var dropdownMenu = d3.select("#dropdownMenu");
-  var dataset = dropdownMenu.property("value");
 
-  var xData = [1, 2, 3, 4, 5];
-  var yData = [];
-
-  if (dataset === 'dataset1') {
-    yData = [1, 2, 4, 8, 16];
-  };
-
-  if (dataset === 'dataset2') {
-    yData = [1, 10, 100, 1000, 10000];
-  };
-
-  var trace = {
-    x: [xData],
-    y: [yData],
-  };
-  Plotly.restyle("plot", trace);
 };
 
-function optionChanged(newSample) {
-    buildMetadata(newSample);
-    buildCharts(newSample);
+function optionChanged(sample) {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var sample = dropdownMenu.property("value");
+    
+    buildMetadata(sample);
+    buildCharts(sample);
   }
   function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
@@ -48,7 +27,7 @@ function optionChanged(newSample) {
       PANEL.append("bbtype: ").text(resultArray.bbtype);
       PANEL.append("wfreq: ").text(resultArray.wfreq);
     });
-   };
-  
+   }
+} 
   
 init();
