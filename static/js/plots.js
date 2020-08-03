26eeedb79df154eefb23389036e5c019f95f6483
Suzanne Wilson <suzanne.wight.wilson@gmail.com>
2020-08-02 21:45:11 -0700

    bar chart

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,4 +1,38 @@
 //Module 12 Belly Button Diversity
+
+function buildCharts(newSample)
+{
+  d3.json("\\static\\js\\samples.json").then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id === newSample);
+    var result = resultArray[0];
+
+    var topResults = result.sort((a, b) => b.sample_values - a.sample_values);
+    
+    var top10Results = topResults.slice(0,10);
+
+    var values = top10Results.sample_values;
+    var labels = top10Results.otu_ids;
+    var display = top10Results.otu_labels;    
+
+    var trace = {
+      x: [values],
+      y: [labels],
+      hovertext: display,
+      type: 'bar',
+      orientation: 'h'
+    };
+     var data = [trace];
+     var layout = {
+      title: "Top Ten Species of Belly Button Bacteria",
+      xaxis: { title: "Sample Values"},
+      yaxis: { title: "Bacterial Species"}
+     };
+     
+     Plotly.newPlot("bar", data, layout);
+  });
+} 
+
 function buildMetadata(newSample) 
 {
     d3.json("\\static\\js\\samples.json").then((data) => {
@@ -15,9 +49,8 @@ function buildMetadata(newSample)
 }   
 
 function optionChanged(newSample) {
- 
     buildMetadata(newSample);
-//       buildCharts(newSample);
+  //  buildCharts(newSample);
   }
 
 function init() {
@@ -33,6 +66,7 @@ function init() {
         });        
         const firstSample = samples[0];
         buildMetadata(firstSample);
+     //   buildCharts(firstSample);
     });  
 }
 
